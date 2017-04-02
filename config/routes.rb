0866451d5c9d6d4027b4e501db55e2fa7c98d739
Marisha Sahay <marishasahay@gmail.com>
2017-04-01 23:47:09 -0700

    style index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "pages#news_index"
+  get "/", to: "pages#abc_news"
   get "/till_now", to: "pages#till_now"
   post '/email_subscriptions', to: 'email_subscriptions#create'
   get '/hotshots', to: "pages#hotshots"
   get '/resources', to: "pages#resources"
+  get '/news', to: 'pages#news_index'
 end
