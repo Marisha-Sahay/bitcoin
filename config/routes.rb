aa14d28cebac86b7f6cb6a4a6174ac73572175aa
Marisha Sahay <marishasahay@gmail.com>
2017-04-06 19:45:36 -0700

    Change home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "pages#abc_news"
+  get "/", to: "pages#news_index"
   get "/till_now", to: "pages#till_now"
   post '/email_subscriptions', to: 'email_subscriptions#create'
   get '/hotshots', to: "pages#hotshots"
