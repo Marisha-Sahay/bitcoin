be79fa1a3dbb079c0de832c95f64244450928b1d
Marisha Sahay <marishasahay@gmail.com>
2017-09-09 16:47:00 -0700

    Update controller actions

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,12 +1,11 @@
 class ResourcesController < ApplicationController
 
   def index
-    @resouces = ["Getting Started","Online Courses","Deep Dive Technical Resources", "Documentaries", "The History of Bitcoin", "Bitcoin Conference Presentations", "Staying Up-to-date", "Miscellaneous Bitcoin Stats", "Buying / Selling bitcoins", "Mining", "Realtime Exchange Data", "Setting up a wallet", "Vendors accepting Bitcoin", "Privacy", "The Lightning Network"]
-    # @resouces = Resource.all
+    @resouces = ResourceCategory.all
   end
 
   def show
-    @resouce = Resource.find_by(id: params[:id])
+    @resouce = ResourceCategory.find_by(id: params[:id])
   end
 
 end
