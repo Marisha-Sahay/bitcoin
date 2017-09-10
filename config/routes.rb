9d60be091918ee9b3996c8c9a350dfdcabb4619f
Marisha Sahay <marishasahay@gmail.com>
2017-09-09 17:12:40 -0700

    Fix style for resource index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/till_now", to: "pages#till_now"
   post "/email_subscriptions", to: "email_subscriptions#create"
   get "/hotshots", to: "pages#hotshots"
-  get "/resources", to: "pages#resources"
+  # get "/resources", to: "pages#resources"
   get "/news", to: "pages#abc_news"
   get "/cafe", to: "pages#cafe"
   
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
   patch '/orders/:id', to: 'orders#update'
 
   # resources routes
-  # get '/resources', to: 'resource_categories#index'
-  # get '/resources/:id', to: 'resource_categories#show'
+  get '/resources', to: 'resource_categories#index'
+  get '/resources/:id', to: 'resource_categories#show'
   
 end
