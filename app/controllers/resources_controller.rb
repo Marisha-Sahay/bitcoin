33836ce4190e4a608d1f998059f2316db0665dbe
Marisha Sahay <marishasahay@gmail.com>
2017-09-17 23:23:43 -0700

    Redirect after saving new resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -8,6 +8,7 @@ class ResourcesController < ApplicationController
     @resource = Resource.new(name: params[:name], description: params[:description], resource_link: params[:resource_link])
     if @resource.save
       @categorized_resource = CategorizedResource.create(resource_id: @resource.id,resource_category_id: params[:resource_category][:resource_category_id])
+      redirect_to "/resource/new"
     end
   end
 
