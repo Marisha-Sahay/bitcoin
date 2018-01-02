2762dcf3df81e5ff81aa6bed04fcd83f6a8e0bc7
Marisha Sahay <marishasahay@gmail.com>
2018-01-01 22:25:18 -0800

    modified database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227181658) do
+ActiveRecord::Schema.define(version: 20180102061256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,9 +165,9 @@ ActiveRecord::Schema.define(version: 20171227181658) do
     t.string   "currency"
     t.integer  "score"
     t.integer  "length"
+    t.string   "video_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "video_type"
   end
 
   create_table "votes", force: :cascade do |t|
