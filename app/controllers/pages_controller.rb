404ec3bb214ec88a7fbce3a30fc2421576a90789
Marisha Sahay <marishasahay@gmail.com>
2018-01-15 10:50:00 -0800

    Updated news api in subscribe page and fixed links in resource show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,9 @@ class PagesController < ApplicationController
     # response = Unirest.get("http://webhose.io/search?token=d8031f4d-87f0-4722-aef6-180d877ebc6d&format=json&q=bitcoin%20cryptocurrency&sort=relevancy").body
     # response = Unirest.get("http://webhose.io/search?token=c015c400-6e3b-4540-a2c0-d643354db3cc&format=json&q=bitcoin%20cryptocurrency&sort=relevancy").body
     # @posts = response['posts'].first(13)
-    @posts = News.distinct(:title).order('published DESC, performance_score DESC').limit(13)
+    # @posts = News.distinct(:title).order('published DESC, performance_score DESC').limit(13)
+    news = Unirest.get("https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=80c38e339dab4d2696f17f9a2ac3b937").body
+    @posts = news['articles']
     @prices = Unirest.get("https://api.coinmarketcap.com/v1/ticker/?limit=10").body
     @contents = FeaturedContent.all.limit 5
     @videos = Video.all.shuffle[0..22]
