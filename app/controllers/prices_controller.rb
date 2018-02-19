3e9eeb10ec596416260048db5b71410f87b4a2b8
Santush Deb Nath <santush.lu.bd@gmail.com>
2018-02-19 07:09:03 +0000

    Save change.

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,8 +1,24 @@
 class PricesController < ApplicationController
 
-  def index
+  def index 
+    @prices = Price.where(["to_symbol = ? and market = ?", "USD", "BitTrex"])
+    # @prices_btc = Price.where(["to_symbol = ? and market = ?", "BTC", "BitTrex"])
+  end
+  def search 
+    price = params[:price];
+    market = params[:market];
+    if price == ''
+      price = 'USD'
+    end
+    if market == ''
+      price = 'BitTrex'
+    end
+    @prices = Price.where(["to_symbol = ? and market = ?", "#{price}", "#{market}"])
+    # @prices_btc = Price.where(["to_symbol = ? and market = ?", "BTC", "BitTrex"])
+    render :index
   end
   def show
+    @price      = Price.find_by(id: params[:id])
   end
 
 end
